dd99d80a5542986ac4afc6addb843c3128cabdde
FIRST_NAME LAST_NAME <mamehussen7@gmail.com>
2021-08-30 10:03:07 +0300

    Add Tip Model

--- models/Tip.js
+++ models/Tip.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 
 const TipSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: ['true', 'Please add a description']
+    },
+    createdBy:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true, 'Please provide user']
+    }
 
-})
+}, {timestamps:true})
 
 module.exports = mongoose.model('Tip', TipSchema)
